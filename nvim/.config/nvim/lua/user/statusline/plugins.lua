1b6eb4de87f5a3f2731b8c06ea306526f5c71063
Forsberg Hedberg Charlie <cfh91@hotmail.com>
2022-05-24 07:18:18 +0200

    added back statusline

--- nvim/.config/nvim/lua/user/statusline/plugins.lua
+++ nvim/.config/nvim/lua/user/statusline/plugins.lua
@@ -3,12 +3,14 @@ return function(use)
         'feline-nvim/feline.nvim',
         config = function()
 
-            -- local components = require'catppuccin.core.integrations.feline'
-            -- components.active[2][1] = nil -- Disable LSP progress
+            local components = require'catppuccin.core.integrations.feline'
+            components.active[2][1] = nil -- Disable LSP progress
 
-            -- require'feline'.setup {
-            --     components = components
-            -- }
+            require'feline'.setup {
+                components = components
+            }
+
+            require'feline'.winbar.setup()
         end
     }
 end
