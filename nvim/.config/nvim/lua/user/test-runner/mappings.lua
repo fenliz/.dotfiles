4bd3f707f57a9701115976ebcce80f73e7f9915a
Forsberg Hedberg Charlie <cfh91@hotmail.com>
2022-06-13 08:04:05 +0200

    more neotest mappings

--- nvim/.config/nvim/lua/user/test-runner/mappings.lua
+++ nvim/.config/nvim/lua/user/test-runner/mappings.lua
@@ -2,9 +2,16 @@ local mapx = require("mapx")
 
 mapx.nname("<leader>t", "Test")
 
-nnoremap("<leader>tt", ':lua require("neotest").run.run()', "<silent>", "Test: Nearest test")
-nnoremap("<leader>tf", ':lua require("neotest").run.run(vim.fn.expand("%"))', "<silent>", "Test: Current file")
+nnoremap("<leader>tt", ':lua require("neotest").run.run()<CR>', "<silent>", "Test: Nearest test")
+nnoremap("<leader>tf", ':lua require("neotest").run.run(vim.fn.expand("%"))<CR>', "<silent>", "Test: Current file")
+nnoremap("<leader>tr", ':lua require("neotest").run.run_last()<CR>', "<silent>", "Test: Rerun test")
+nnoremap("<leader>ts", ':lua require("neotest").summary.toggle()', "<silent>", "Test Summary: Toggle")
 
-nnoremap("<leader>td", ':lua require("neotest").run.run({ strategy = "dap" })', "<silent>", "Test: Debug nearest test")
-nnoremap("<leader>ts", ':lua require("neotest").run.stop()', "<silent>", "Test: Stop nearest test")
-nnoremap("<leader>ta", ':lua require("neotest").run.attach()', "<silent>", "Test: Attach nearest test")
+nnoremap(
+  "<leader>td",
+  ':lua require("neotest").run.run({ strategy = "dap" })<CR>',
+  "<silent>",
+  "Test: Debug nearest test"
+)
+nnoremap("<leader>ts", ':lua require("neotest").run.stop()<CR>', "<silent>", "Test: Stop nearest test")
+nnoremap("<leader>ta", ':lua require("neotest").run.attach()<CR>', "<silent>", "Test: Attach nearest test")
