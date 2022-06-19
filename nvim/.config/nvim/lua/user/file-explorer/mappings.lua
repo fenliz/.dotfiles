75d088561b14b0bc83d8c861183d3bfbd81b94d4
Forsberg Hedberg Charlie <cfh91@hotmail.com>
2022-06-19 16:09:31 +0200

    update nvim-tree mappings

--- nvim/.config/nvim/lua/user/file-explorer/mappings.lua
+++ nvim/.config/nvim/lua/user/file-explorer/mappings.lua
@@ -1,2 +1,2 @@
-nnoremap("<leader>ee", ":NvimTreeOpen<CR>", "<silent>", "Open file explorer")
+nnoremap("<leader>ee", ":NvimTreeFindFile<CR>", "<silent>", "Open file explorer")
 nnoremap("<leader>ec", ":NvimTreeClose<CR>", "<silent>", "Close file explorer")
--- nvim/.config/nvim/lua/user/file-explorer/plugins.lua
+++ nvim/.config/nvim/lua/user/file-explorer/plugins.lua
@@ -1,37 +1,33 @@
 return function(use)
-	use({
-		"kyazdani42/nvim-tree.lua",
-		config = function()
-			local nvim_tree = require("nvim-tree")
-			local nvim_tree_cb = require("nvim-tree.config").nvim_tree_callback
+  use({
+    "kyazdani42/nvim-tree.lua",
+    config = function()
+      local nvim_tree = require("nvim-tree")
+      local nvim_tree_cb = require("nvim-tree.config").nvim_tree_callback
 
-			nvim_tree.setup({
-				update_focused_file = {
-					enable = true,
-				},
-				view = {
-					width = 40,
-					hide_root_folder = true,
-					mappings = {
-						list = {
-							{ key = "t", cb = nvim_tree_cb("tabnew") },
-							{ key = "l", cb = nvim_tree_cb("edit") },
-							{ key = "h", cb = nvim_tree_cb("parent_node") },
-						},
-					},
-				},
-				actions = {
-					open_file = {
-						resize_window = true,
-					},
-				},
-				renderer = {
-					indent_markers = {
-						enable = true,
-					},
-					highlight_opened_files = "all",
-				},
-			})
-		end,
-	})
+      nvim_tree.setup({
+        view = {
+          width = 40,
+          hide_root_folder = true,
+          mappings = {
+            list = {
+              { key = "l", cb = nvim_tree_cb("edit") },
+              { key = "h", cb = nvim_tree_cb("close_node") },
+            },
+          },
+        },
+        actions = {
+          open_file = {
+            resize_window = true,
+          },
+        },
+        renderer = {
+          indent_markers = {
+            enable = true,
+          },
+          highlight_opened_files = "all",
+        },
+      })
+    end,
+  })
 end
