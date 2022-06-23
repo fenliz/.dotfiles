10952aef82b16aead1f5e6ab87469370d7d9d0b9
Forsberg Hedberg Charlie <cfh91@hotmail.com>
2022-06-23 07:39:28 +0200

    disable test discovery

--- nvim/.config/nvim/lua/user/test-runner/plugins.lua
+++ nvim/.config/nvim/lua/user/test-runner/plugins.lua
@@ -15,6 +15,9 @@ return function(use)
     },
     config = function()
       require("neotest").setup({
+        discovery = {
+          enabled = false,
+        },
         adapters = {
           -- require("neotest-jest"),
           require("neotest-vim-test")({
