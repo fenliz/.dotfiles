7afbf7db355d8a2b3ed451ad136cf177800119b3
Forsberg Hedberg Charlie <cfh91@hotmail.com>
2022-06-23 07:38:07 +0200

    formatting

--- nvim/.config/nvim/lua/user/terminal/mappings.lua
+++ nvim/.config/nvim/lua/user/terminal/mappings.lua
@@ -1,19 +1,19 @@
 local toggle_terminal_augroup = vim.api.nvim_create_augroup("ToggleTerminal", {})
 
 local function set_terminal_keymaps()
-  local opts = { noremap = true }
-  vim.api.nvim_buf_set_keymap(0, "t", "<Esc>", [[<C-\><C-n>]], opts)
-  vim.api.nvim_buf_set_keymap(0, "t", "<C-h>", [[<C-\><C-n><C-W>h]], opts)
-  vim.api.nvim_buf_set_keymap(0, "t", "<C-j>", [[<C-\><C-n><C-W>j]], opts)
-  vim.api.nvim_buf_set_keymap(0, "t", "<C-k>", [[<C-\><C-n><C-W>k]], opts)
-  vim.api.nvim_buf_set_keymap(0, "t", "<C-l>", [[<C-\><C-n><C-W>l]], opts)
+	local opts = { noremap = true }
+	vim.api.nvim_buf_set_keymap(0, "t", "<Esc>", [[<C-\><C-n>]], opts)
+	vim.api.nvim_buf_set_keymap(0, "t", "<C-h>", [[<C-\><C-n><C-W>h]], opts)
+	vim.api.nvim_buf_set_keymap(0, "t", "<C-j>", [[<C-\><C-n><C-W>j]], opts)
+	vim.api.nvim_buf_set_keymap(0, "t", "<C-k>", [[<C-\><C-n><C-W>k]], opts)
+	vim.api.nvim_buf_set_keymap(0, "t", "<C-l>", [[<C-\><C-n><C-W>l]], opts)
 end
 
 vim.api.nvim_clear_autocmds({ group = toggle_terminal_augroup })
 vim.api.nvim_create_autocmd("TermOpen", {
-  pattern = { "term://*" },
-  group = toggle_terminal_augroup,
-  callback = set_terminal_keymaps,
+	pattern = { "term://*" },
+	group = toggle_terminal_augroup,
+	callback = set_terminal_keymaps,
 })
 
 nnoremap("<C-t>", '<Cmd>exe v:count1 . "ToggleTerm"<CR>')
--- nvim/.config/nvim/lua/user/terminal/utils.lua
+++ nvim/.config/nvim/lua/user/terminal/utils.lua
@@ -1,20 +1,19 @@
 local Terminal = require("toggleterm.terminal").Terminal
 local Lazygit = Terminal:new({
-  cmd = "lazygit",
-  hidden = true,
-  direction = "float",
-  on_open = function(term)
-    vim.cmd("startinsert!")
-    vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", { noremap = true, silent = true })
-    vim.api.nvim_buf_set_keymap(term.bufnr, "t", "<Esc>", "<cmd>close<CR>", { noremap = true, silent = true })
-    vim.api.nvim_buf_set_keymap(term.bufnr, "n", "<Esc>", "<cmd>close<CR>", { noremap = true, silent = true })
-  end,
+	cmd = "lazygit",
+	hidden = true,
+	direction = "float",
+	on_open = function(term)
+		vim.cmd("startinsert!")
+		vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", { noremap = true, silent = true })
+		vim.api.nvim_buf_set_keymap(term.bufnr, "n", "<Esc>", "<cmd>close<CR>", { noremap = true, silent = true })
+	end,
 })
 
 M = {}
 
 M.toggle_lazygit = function()
-  Lazygit:toggle()
+	Lazygit:toggle()
 end
 
 return M
