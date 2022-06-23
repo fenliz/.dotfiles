048e0f1845f9f3d42905116c468c3c1b7d9551bb
Forsberg Hedberg Charlie <cfh91@hotmail.com>
2022-06-23 07:38:48 +0200

    remove right-aligned git blame

--- nvim/.config/nvim/lua/user/version-control/plugins.lua
+++ nvim/.config/nvim/lua/user/version-control/plugins.lua
@@ -1,13 +1,10 @@
 return function(use)
-	use({
-		"lewis6991/gitsigns.nvim",
-		config = function()
-			require("gitsigns").setup({
-				current_line_blame = true,
-				current_line_blame_opts = {
-					virt_text_pos = "right_align",
-				},
-			})
-		end,
-	})
+  use({
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      require("gitsigns").setup({
+        current_line_blame = true,
+      })
+    end,
+  })
 end
