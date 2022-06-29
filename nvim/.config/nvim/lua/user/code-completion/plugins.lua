26fa7751db39c6a9d4e35b42b097edde08199355
Forsberg Hedberg Charlie <cfh91@hotmail.com>
2022-06-29 06:20:36 +0200

    autotag plugin

--- nvim/.config/nvim/lua/user/code-completion/plugins.lua
+++ nvim/.config/nvim/lua/user/code-completion/plugins.lua
@@ -84,14 +84,14 @@ return function(use)
         },
       })
 
-      cmp.setup.cmdline(":", {
-        mapping = cmp.mapping.preset.cmdline(),
-        sources = cmp.config.sources({
-          { name = "path" },
-        }, {
-          { name = "cmdline" },
-        }),
-      })
+      -- cmp.setup.cmdline(":", {
+      --   mapping = cmp.mapping.preset.cmdline(),
+      --   sources = cmp.config.sources({
+      --     { name = "path" },
+      --   }, {
+      --     { name = "cmdline" },
+      --   }),
+      -- })
 
       cmp.setup.cmdline("/", {
         mapping = cmp.mapping.preset.cmdline(),
