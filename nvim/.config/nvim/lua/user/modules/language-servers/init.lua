244c0902331a881de2af22da30d9a5c02ae08731
Forsberg Hedberg Charlie <cfh91@hotmail.com>
2022-09-05 20:08:15 +0200

    use notifier instead of notify

--- nvim/.config/nvim/lua/user/modules/language-servers/init.lua
+++ nvim/.config/nvim/lua/user/modules/language-servers/init.lua
@@ -109,15 +109,6 @@ M.plugins = function(use)
 	})
 
 	use({
-
-		"j-hui/fidget.nvim",
-		config = function()
-			require("fidget").setup({})
-		end,
-	})
-
-	use({
-
 		"ray-x/lsp_signature.nvim",
 		config = function()
 			require("lsp_signature").setup({ hint_enable = false })
--- nvim/.config/nvim/lua/user/modules/notifications.lua
+++ nvim/.config/nvim/lua/user/modules/notifications.lua
@@ -1,16 +1,23 @@
 local M = {}
 
 M.plugins = function(use)
-  use({
-    "rcarriga/nvim-notify",
-    config = function()
-      vim.notify = require("notify")
-      require("notify").setup({
-        render = "minimal",
-        timeout = 1000,
-      })
-    end,
-  })
+	--[[ use({ ]]
+	--[[   "rcarriga/nvim-notify", ]]
+	--[[   config = function() ]]
+	--[[     vim.notify = require("notify") ]]
+	--[[     require("notify").setup({ ]]
+	--[[       render = "minimal", ]]
+	--[[       timeout = 1000, ]]
+	--[[     }) ]]
+	--[[   end, ]]
+	--[[ }) ]]
+
+	use({
+		"vigoux/notifier.nvim",
+		config = function()
+			require("notifier").setup({})
+		end,
+	})
 end
 
 return M
