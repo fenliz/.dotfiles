18d82fdf68128dbd151c7fb71fa1e2287d941898
Forsberg Hedberg Charlie <cfh91@hotmail.com>
2022-11-30 06:27:34 +0100

    fix typescript nullcheck

--- nvim/.config/nvim/lua/user/modules/language-servers/typescript.lua
+++ nvim/.config/nvim/lua/user/modules/language-servers/typescript.lua
@@ -1,23 +1,23 @@
 return function(opts)
-  local on_attach = opts.on_attach
+	local on_attach = opts.on_attach
 
-  -- Disable builtin formatting in favor of null-ls
-  opts.on_attach = function(client, bufnr)
-    on_attach(client, bufnr)
-    client.server_capabilities.documentFormattingProvider = false
-  end
+	-- Disable builtin formatting in favor of null-ls
+	opts.on_attach = function(client, bufnr)
+		on_attach(client, bufnr)
+		client.server_capabilities.documentFormattingProvider = false
+	end
 
-  opts.handlers = {
-    ["textDocument/definition"] = function(err, result, method, ...)
-      if vim.tbl_islist(result) and #result > 1 then
-        result = vim.tbl_filter(function(v)
-          return not string.find(v.uri, "react/index.d.ts")
-        end, result)
-      end
+	opts.handlers = {
+		["textDocument/definition"] = function(err, result, method, ...)
+			if vim.tbl_islist(result) and #result > 1 then
+				result = vim.tbl_filter(function(v)
+					return v.uri ~= nil and not string.find(v.uri, "react/index.d.ts")
+				end, result)
+			end
 
-      vim.lsp.handlers["textDocument/definition"](err, result, method, ...)
-    end,
-  }
+			vim.lsp.handlers["textDocument/definition"](err, result, method, ...)
+		end,
+	}
 
-  return opts
+	return opts
 end
