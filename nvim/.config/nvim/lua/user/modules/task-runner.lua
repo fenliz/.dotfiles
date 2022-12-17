42b999896e062b1bef5aefc4275126d731b5957e
Forsberg Hedberg Charlie <cfh91@hotmail.com>
2022-12-17 22:57:56 +0100

    replace tasks with overseer

--- nvim/.config/nvim/lua/user/modules/task-runner.lua
+++ nvim/.config/nvim/lua/user/modules/task-runner.lua
@@ -1,41 +1,30 @@
 local M = {}
 
 M.mappings = function()
-	nnoremap("<leader>qq", "<cmd>Telescope tasks specs<cr>", "<silent>", "Task: List")
-	nnoremap("<leader>qr", "<cmd>Telescope tasks running<cr>", "<silent>", "Task: Running")
+	vim.api.nvim_create_user_command("OverseerRestartLast", function()
+		local overseer = require("overseer")
+		local tasks = overseer.list_tasks({ recent_first = true })
+		if vim.tbl_isempty(tasks) then
+			vim.notify("No recent tasks found", vim.log.levels.WARN)
+		else
+			overseer.run_action(tasks[1], "restart")
+		end
+	end, {})
 
-	nnoremap("<leader>ql", function()
-		require("tasks").run_last()
-	end, "<silent>", "Task: Run last")
+	nnoremap("<leader>qq", "<cmd>OverseerRun<cr>", "<silent>", "Task: List")
+	nnoremap("<leader>ql", "<cmd>OverseerRestartLast<cr>", "<silent>", "Task: Run last")
 end
 
 M.plugins = function(use)
 	use({
-		"GustavoKatel/tasks.nvim",
-		requires = { "nvim-lua/plenary.nvim" },
+		"stevearc/overseer.nvim",
 		config = function()
-			require("tasks").setup({
-				runners = {
-					npm_toggleterm = {
-						create_task = function(_, spec)
-							return require("tasks.lib.task"):new(function()
-								require("toggleterm").exec(table.concat(spec.cmd, " "))
-							end)
-						end,
-					},
+			require("overseer").setup({
+				strategy = {
+					"toggleterm",
+					open_on_start = true,
 				},
-				sources = {
-					npm = require("tasks.sources.npm"),
-					vscode = require("tasks.sources.tasksjson"),
-				},
-				router = function(_, _, _, source)
-					if source == "npm" then
-						return "npm_toggleterm"
-					end
-				end,
 			})
-
-			require("telescope").load_extension("tasks")
 		end,
 	})
 end
