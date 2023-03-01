a4f0b3eee70cc14e996e16a940935e54b49f4558
Forsberg Hedberg Charlie <cfh91@hotmail.com>
2023-03-01 08:14:51 +0100

    disable neoscroll

--- nvim/.config/nvim/lua/user/modules/navigation.lua
+++ nvim/.config/nvim/lua/user/modules/navigation.lua
@@ -1,26 +1,26 @@
 return {
 	{
 		"tpope/vim-surround",
-		lazy = false
+		lazy = false,
 	},
 	{
 		"ggandor/lightspeed.nvim",
 		event = "BufReadPre",
 	},
-	{
-		"karb94/neoscroll.nvim",
-		keys = { "<C-u>", "<C-d>", "gg", "G" },
-		config = function()
-			require("neoscroll").setup({
-				mappings = { "<C-u>", "<C-d>" },
-			})
-
-			require("neoscroll.config").set_mappings({
-				["<C-u>"] = { "scroll", { "-vim.wo.scroll", "true", "80" } },
-				["<C-d>"] = { "scroll", { "vim.wo.scroll", "true", "80" } },
-			})
-		end,
-	},
+	-- {
+	-- 	"karb94/neoscroll.nvim",
+	-- 	keys = { "<C-u>", "<C-d>", "gg", "G" },
+	-- 	config = function()
+	-- 		require("neoscroll").setup({
+	-- 			mappings = { "<C-u>", "<C-d>" },
+	-- 		})
+	--
+	-- 		require("neoscroll.config").set_mappings({
+	-- 			["<C-u>"] = { "scroll", { "-vim.wo.scroll", "true", "80" } },
+	-- 			["<C-d>"] = { "scroll", { "vim.wo.scroll", "true", "80" } },
+	-- 		})
+	-- 	end,
+	-- },
 	{
 		"ofirgall/open.nvim",
 		keys = "gx",
