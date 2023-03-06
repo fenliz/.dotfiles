548f590762fdb8cc3503b6c61de135b7d781ad9a
Forsberg Hedberg Charlie <cfh91@hotmail.com>
2023-03-06 16:43:32 +0100

    notification at bottom

--- nvim/.config/nvim/lua/user/modules/ui.lua
+++ nvim/.config/nvim/lua/user/modules/ui.lua
@@ -1,18 +1,12 @@
--- vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
--- 	border = "rounded",
--- 	silent = true,
--- })
-
-vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
-	border = "rounded",
-})
-
 return {
 	{
 		"rcarriga/nvim-notify",
 		event = "BufReadPre",
 		config = function()
-			require("notify").setup()
+			require("notify").setup({
+				render = "compact",
+				top_down = false,
+			})
 		end,
 	},
 	{
