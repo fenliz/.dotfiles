1b6fb39ebb2d51355312ca2f274fb38af114e6a1
Forsberg Hedberg Charlie <cfh91@hotmail.com>
2023-03-15 07:25:55 +0100

    cleanup version-control

--- nvim/.config/nvim/lua/user/modules/version-control.lua
+++ nvim/.config/nvim/lua/user/modules/version-control.lua
@@ -2,40 +2,18 @@ return {
 	{
 		"lewis6991/gitsigns.nvim",
 		event = "BufReadPre",
-		keys = {
-			{
-				"ä",
-				function()
-					require("gitsigns").next_hunk()
-				end,
-			},
-			{
-				"ö",
-				function()
-					require("gitsigns").prev_hunk()
-				end,
-			},
+		opts = {
+			current_line_blame = true,
 		},
-		config = function()
-			require("gitsigns").setup({
-				current_line_blame = true,
-			})
-		end,
 	},
 	{
 		"sindrets/diffview.nvim",
 		keys = {
 			{ "<leader>gh", "<cmd>DiffviewFileHistory %<cr>", desc = "Git: File History" },
 		},
-		config = function()
-			require("diffview").setup()
-		end,
 	},
 	{
 		"akinsho/git-conflict.nvim",
 		event = "BufReadPre",
-		config = function()
-			require("git-conflict").setup()
-		end,
 	},
 }
