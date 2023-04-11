94661ae8110c00e98f098b1c1cd78c447fb8dfca
Forsberg Hedberg Charlie <cfh91@hotmail.com>
2023-04-11 07:34:24 +0200

    fix typescript go to definition

--- nvim/.config/nvim/lua/user/modules/language-servers/languages/typescript.lua
+++ nvim/.config/nvim/lua/user/modules/language-servers/languages/typescript.lua
@@ -3,14 +3,23 @@ local M = {}
 M.setup = function(opts)
 	require("typescript").setup({
 		server = {
+			handlers = {
+				["textDocument/definition"] = function(err, result, ...)
+					if vim.tbl_islist(result) and #result > 1 then
+						result = vim.tbl_filter(function(v)
+							return not string.find(v.targetUrl, "react/index.d.ts")
+						end, result)
+					end
+
+					vim.lsp.handlers["textDocument/definition"](err, result, ...)
+				end,
+			},
 			on_attach = function(client, bufnr)
 				opts.on_attach(client, bufnr)
 
 				vim.keymap.set("n", "go", "<cmd>TypescriptAddMissingImports<cr>", { buffer = bufnr })
 				vim.keymap.set("n", "gO", "<cmd>TypescriptOrganizeImports<cr>", { buffer = bufnr })
 				vim.keymap.set("n", "gI", "<cmd>TypescriptRenameFile<cr>", { buffer = bufnr })
-
-				-- vim.keymap.set("n", "gd", "<cmd>TypescriptGoToSourceDefinition<cr>", { buffer = bufnr })
 			end,
 		},
 	})
