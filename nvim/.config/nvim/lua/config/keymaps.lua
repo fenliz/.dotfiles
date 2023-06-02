b5bf6fd32fce5458498df47cfced50a3fc64ada7
Forsberg Hedberg Charlie <cfh91@hotmail.com>
2023-06-02 15:54:19 +0200

    keymaps for changing text with ENTER from visual and normal mode

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,1 +1,4 @@
 -- Default keymaps: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+
+vim.keymap.set("v", "<cr>", "c", { silent = true })
+vim.keymap.set("n", "<cr>", "ciw", { silent = true })
