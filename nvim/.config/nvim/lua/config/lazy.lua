81d8eda3cb3f31cc11d45e7a90ce20e9757972a1
Forsberg Hedberg Charlie <cfh91@hotmail.com>
2023-08-07 06:17:40 +0200

    remove flash.nvim since its default now

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -9,7 +9,6 @@ require("lazy").setup({
   spec = {
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
 
-    { import = "lazyvim.plugins.extras.editor.flash" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
