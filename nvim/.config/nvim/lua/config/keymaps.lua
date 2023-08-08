2ecbe06840e37cf82cf241f65e69a6458330806d
Forsberg Hedberg Charlie <cfh91@hotmail.com>
2023-08-08 11:05:57 +0200

    remove enter keymaps

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,4 +1,1 @@
 -- Default keymaps: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
-
-vim.keymap.set("v", "<cr>", "c", { silent = true })
-vim.keymap.set("n", "<cr>", "ciw", { silent = true })
