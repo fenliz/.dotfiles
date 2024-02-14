90fac2449bcd2c386312b7298ae46423e04b3313
Forsberg Hedberg Charlie <cfh91@hotmail.com>
2024-02-14 07:16:34 +0100

    update

---
+++ nvim/.config/nvim/lua/plugins/git_gh-blame.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "dlvhdr/gh-blame.nvim",
+    dependencies = { "nvim-lua/plenary.nvim", "MunifTanjim/nui.nvim" },
+    keys = {
+      { "<leader>gb", "<cmd>GhBlameCurrentLine<cr>", desc = "Blame current line" },
+    },
+  },
+}
