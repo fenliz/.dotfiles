624b88d007042d45fad796a3543b8044562a7250
Forsberg Hedberg Charlie <cfh91@hotmail.com>
2024-05-23 11:04:47 +0200

    add copilot chat prompts

--- nvim/.config/nvim/lua/plugins/ai_copilot.lua
+++ nvim/.config/nvim/lua/plugins/ai_copilot.lua
@@ -22,7 +22,32 @@ return {
       { "nvim-lua/plenary.nvim" },
     },
     lazy = false,
+    opts = {
+      prompts = {
+        -- Code related prompts
+        Explain = "Please explain how the following code works.",
+        Review = "Please review the following code and provide suggestions for improvement.",
+        Tests = "Please explain how the selected code works, then generate unit tests for it.",
+        Refactor = "Please refactor the following code to improve its clarity and readability.",
+        FixCode = "Please fix the following code to make it work as intended.",
+        FixError = "Please explain the error in the following text and provide a solution.",
+        BetterNamings = "Please provide better names for the following variables and functions.",
+        -- Text related prompts
+        Summarize = "Please summarize the following text.",
+        Spelling = "Please correct any grammar and spelling errors in the following text.",
+        Wording = "Please improve the grammar and wording of the following text.",
+        Concise = "Please rewrite the following text to make it more concise.",
+      },
+    },
     keys = {
+      {
+        "<leader>aa",
+        function()
+          local actions = require("CopilotChat.actions")
+          require("CopilotChat.integrations.telescope").pick(actions.prompt_actions())
+        end,
+        desc = "Prompts",
+      },
       {
         "<leader>aq",
         function()
@@ -33,10 +58,6 @@ return {
         end,
         desc = "Quick chat",
       },
-      { "<leader>ae", "<cmd>CopilotChatExplain<cr>", mode = "x", desc = "Explain code" },
-      { "<leader>at", "<cmd>CopilotChatTests<cr>", mode = "x", desc = "Generate tests" },
-      { "<leader>ar", "<cmd>CopilotChatReview<cr>", mode = "x", desc = "Review code" },
-      { "<leader>af", "<cmd>CopilotChatFixDiagnostic", mode = "x", desc = "Fix diagnostic" },
     },
   },
 }
