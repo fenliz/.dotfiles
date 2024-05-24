fe3434b1c5c91d3858615549772cae5372783c35
Forsberg Hedberg Charlie <cfh91@hotmail.com>
2024-05-25 00:01:50 +0200

    playwright neotest support

--- nvim/.config/nvim/lua/plugins/test_neotest.lua
+++ nvim/.config/nvim/lua/plugins/test_neotest.lua
@@ -3,11 +3,20 @@ return {
     "nvim-neotest/neotest",
     dependencies = {
       "nvim-neotest/neotest-jest",
+      "thenbe/neotest-playwright",
     },
-    opts = {
-      adapters = {
-        "neotest-jest",
-      },
-    },
+    config = function()
+      require("neotest").setup({
+        adapters = {
+          require("neotest-jest"),
+          require("neotest-playwright").adapter({
+            options = {
+              persist_project_selection = true,
+              enable_dynamic_test_discovery = true,
+            },
+          }),
+        },
+      })
+    end,
   },
 }
