916b655795794c6ea4a4e7d3c7a0a42a4740bfd6
Forsberg Hedberg Charlie <cfh91@hotmail.com>
2024-05-24 23:01:54 +0200

    more aliases

--- zsh/zshrc/alias.zsh
+++ zsh/zshrc/alias.zsh
@@ -1,10 +1,6 @@
+# General
 alias cat='bat'
 alias cd='z'
-
-# General
-alias ll='ls -alF'
-alias la='ls -A'
-alias l='ls -CF'
 
 alias ..='cd ..'
 alias ...='cd ../..'
@@ -14,3 +10,12 @@ alias cdr='cd $(git rev-parse --show-toplevel)'
 # Neovim
 alias vim=nvim
 alias nv=nvim
+
+# Git
+alias g=lazygit
+
+# Files
+alias f=yazi
+alias ll='ls -alF'
+alias la='ls -A'
+alias l='ls -CF'
