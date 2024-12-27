89dfde8f93846c2f49c02c915a666e178c3f8dfc
Forsberg Hedberg Charlie <cfh91@hotmail.com>
2024-12-27 23:39:08 +0100

    disable smooth scrolling

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -4,5 +4,8 @@
 -- Always keep 8 lines above and below the cursor
 vim.opt.scrolloff = 8
 
+-- Disable smooth scrolling
+vim.opt.smoothscroll = false
+
 -- Autocomplete options
 vim.opt.completeopt = "menu,menuone,noselect,noinsert"
