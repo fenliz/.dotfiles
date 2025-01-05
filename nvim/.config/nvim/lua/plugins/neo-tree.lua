e1ed7c086ba7043166b865693d9afe7ddca7dae7
Forsberg Hedberg Charlie <cfh91@hotmail.com>
2025-01-05 21:14:40 +0100

    revert copilot

--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -3,7 +3,41 @@ local cwd_in_dotfiles = string.match(cwd, ".dotfiles")
 
 return {
   {
+    "s1n7ax/nvim-window-picker",
+    version = "2.*",
+    opts = {
+      hint = "floating-big-letter",
+      filter_rules = {
+        include_current_win = false,
+        autoselect_one = true,
+        bo = {
+          filetype = {
+            "blink-cmp-documentation",
+            "blink-cmp-menu",
+            "blink-cmp-signature",
+            "edgy",
+            "neo-tree",
+            "noice",
+            "notify",
+            "nvimtree",
+            "smear-cursor",
+            "snacks_dashboard",
+            "snacks_notif",
+            "snacks_terminal",
+            "toggleterm",
+            "trouble",
+            "tutor",
+          },
+          buftype = { "terminal", "quickfix" },
+        },
+      },
+    },
+  },
+  {
     "nvim-neo-tree/neo-tree.nvim",
+    dependencies = {
+      "s1n7ax/nvim-window-picker",
+    },
     opts = {
       filesystem = {
         filtered_items = {
@@ -13,7 +47,8 @@ return {
       },
       window = {
         mappings = {
-          ["l"] = "open",
+          ["<cr>"] = "open_with_window_picker",
+          ["l"] = "open_with_window_picker",
           ["h"] = "close_node",
         },
       },
